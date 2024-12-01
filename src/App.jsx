7602313b8aac6e95bd197193302eaaaeed86551b
Salman-sajib <sfsajib@gmail.com>
2024-12-01 12:27:33 +0600

    added a stagger animation to some circles

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,10 @@ import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 
 function App() {
-  const container = useRef();
+  const firstContainer = useRef();
   const circle = useRef();
   const animateButton = useRef();
-  const buttonText = useRef();
+  const secondContainer = useRef();
 
   const [isAnimating, setIsAnimating] = useState(false);
 
@@ -31,41 +31,29 @@ function App() {
     });
   });
 
-  /*
-  // different approach
-  useGSAP((context, contextSafe) => {
-    const onClickAnimation = contextSafe(() => {
-      // rotate the box with selector
-      gsap.to(".box", { rotate: "+=360", duration: 1 });
-      // rotate the circle with ref
-      gsap.to(circle.current, { rotate: "-=360", duration: 1 });
-
-      console.log(`context on click: ${context.data.length}`);
+  useGSAP(() => {
+    gsap.from(secondContainer.current.children, {
+      opacity: 0,
+      y: 100,
+      stagger: 0.2,
+      duration: 1,
+      ease: "power3",
     });
-
-    animateButton.current.addEventListener("click", onClickAnimation);
-
-    console.log(`context on mount: ${context.data.length}`);
-
-    return () => {
-      animateButton.current.removeEventListener("click", onClickAnimation);
-    };
   });
-  */
 
   return (
     <div className="bg-zinc-950 text-gray-100 min-h-dvh grid place-content-center">
       <div className=" absolute top-[100px] left-1/2 -translate-x-1/2">
         <button
-          // ref={animateButton}
+          ref={animateButton}
           onClick={onClickAnimation}
           className="border-2 outline-none border-gray-100 font-semibold text-gray-100 px-9 py-3 rounded-full hover:bg-gray-100 hover:text-zinc-900 transition-all duration-200 overflow-hidden"
         >
-          <span>Click To Animate</span>
+          Click To Animate
         </button>
       </div>
-      <div className="flex flex-col items-center gap-10">
-        <div ref={container} className="flex gap-10">
+      <div className="flex flex-col items-center gap-[100px]">
+        <div ref={firstContainer} className="flex gap-10">
           <div className="box w-[100px] h-[100px] bg-gradient-to-br from-blue-200 to-blue-400 rounded-lg text-zinc-900 text-[16px] font-medium flex items-center justify-center">
             Selector
           </div>
@@ -75,6 +63,17 @@ function App() {
           >
             Ref
           </div>
+        </div>
+
+        <div ref={secondContainer} className="flex items-center gap-5">
+          {[...Array(5)].map((_, index) => (
+            <div
+              key={index}
+              className="w-[70px] h-[70px] rounded-full bg-gradient-to-tr from-lime-200 to-lime-500 flex items-center justify-center text-zinc-900 font-semibold text-lg"
+            >
+              {index + 1}
+            </div>
+          ))}
         </div>
       </div>
     </div>
