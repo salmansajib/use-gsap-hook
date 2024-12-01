00b6a489fd02b79cc780a877624eea1f4d1b20fc
Salman-sajib <sfsajib@gmail.com>
2024-12-01 13:40:34 +0600

    added a dot that follows the cursor

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 
@@ -7,10 +7,10 @@ function App() {
   const circle = useRef();
   const animateButton = useRef();
   const secondContainer = useRef();
-
+  const dotRef = useRef();
   const [isAnimating, setIsAnimating] = useState(false);
 
-  // common approach
+  // most common approach in react on click animation with auto clean-up
   const { contextSafe } = useGSAP();
 
   const onClickAnimation = contextSafe(() => {
@@ -31,21 +31,104 @@ function App() {
     });
   });
 
+  // element that animates on mount
   useGSAP(() => {
+    // animate the button
+    gsap.from(animateButton.current, {
+      opacity: 0,
+      y: -110,
+      scale: 0.3,
+      duration: 1.5,
+      delay: 0.2,
+      ease: "power3",
+    });
+
+    // animate the box
+    gsap.from(".box", {
+      opacity: 0,
+      x: -200,
+      duration: 1,
+      delay: 0.4,
+    });
+
+    // animate the box
+    gsap.from(".circle", {
+      opacity: 0,
+      x: 200,
+      duration: 1,
+      delay: 0.8,
+    });
+
+    // animate the circles
     gsap.from(secondContainer.current.children, {
       opacity: 0,
       y: 100,
+      scale: 0,
       stagger: 0.2,
       duration: 1,
+      delay: 1.2,
       ease: "power3",
     });
   });
 
+  // button animation on mouse move
+  const handleButtonMouseMove = (e) => {
+    const rect = animateButton.current.getBoundingClientRect();
+    const x = e.clientX - rect.left - rect.width / 2;
+    const y = e.clientY - rect.top - rect.height / 2;
+
+    gsap.to(animateButton.current, {
+      x: x * 0.2, // Adjust intensity of movement
+      y: y * 0.2, // Adjust intensity of movement
+      duration: 0.3,
+    });
+  };
+
+  // return the button to it's current position on mouse leave
+  const handleButtonMouseLeave = () => {
+    gsap.to(animateButton.current, {
+      x: 0,
+      y: 0,
+      duration: 0.3,
+    });
+  };
+
+  // handle animation on dot that follows the cursor
+  const handleDotMouseMove = (e) => {
+    gsap.to(dotRef.current, {
+      x: e.clientX,
+      y: e.clientY,
+      duration: 0.3,
+      delay: 0.05,
+      ease: "power3",
+    });
+  };
+
+  // animate the dot with clean-up
+  useEffect(() => {
+    window.addEventListener("mousemove", handleDotMouseMove);
+
+    return () => {
+      window.removeEventListener("mousemove", handleDotMouseMove);
+    };
+  }, []);
+
   return (
-    <div className="bg-zinc-950 text-gray-100 min-h-dvh grid place-content-center">
-      <div className=" absolute top-[100px] left-1/2 -translate-x-1/2">
+    <div className="bg-zinc-950 text-gray-100 min-h-dvh grid place-content-center overflow-hidden">
+      {/* Dot Element */}
+      <div
+        ref={dotRef}
+        className="z-10 absolute -top-3 w-5 h-5 border-[3px] border-gray-100  rounded-full pointer-events-none"
+        style={{ transform: "translate(-50%, -50%)" }}
+      ></div>
+
+      <div
+        ref={animateButton}
+        onMouseMove={handleButtonMouseMove}
+        onMouseLeave={handleButtonMouseLeave}
+        className=" absolute top-[100px] left-1/2 -translate-x-1/2"
+      >
         <button
-          ref={animateButton}
           onClick={onClickAnimation}
           className="border-2 outline-none border-gray-100 font-semibold text-gray-100 px-9 py-3 rounded-full hover:bg-gray-100 hover:text-zinc-900 transition-all duration-200 overflow-hidden"
         >
@@ -65,7 +148,7 @@ function App() {
           </div>
         </div>
 
-        <div ref={secondContainer} className="flex items-center gap-5">
+        <div ref={secondContainer} className="flex items-center gap-2">
           {[...Array(5)].map((_, index) => (
             <div
               key={index}
